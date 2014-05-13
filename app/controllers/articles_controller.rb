90024c374bbd644b375f1a353165a3850fab1c77
Ram Bhaskar <rambhask@mit.edu>
2014-05-12 20:40:55 -0400

    added create new functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+	include ArticlesHelper
+
 	def index
 		@articles = Article.all
 	end
@@ -8,4 +10,19 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 	end
 
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+  	@article.save
+  	redirect_to article_path(@article)
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destory
+		redirect_to articles_path
+	end
 end
